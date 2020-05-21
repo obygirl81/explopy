c851ba064ec271e89423c435f350d0ecc34f663c
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-20 23:20:46 -0600

    Initial commit

---
+++ fun.py
@@ -0,0 +1,11 @@
+print('Hello Oby! Welcome to learning python')
+# asking for information from user. Done using the input function
+name = input('Please enter your name ')
+#  using \n to insert blank line to make code more readable
+print('Here is the first blank line\n')
+# another way to print or display a blank line is shown below:
+print('There you go ' + name)
+print()
+# a special character to insert a new line and can be placed in between strings
+print('I love coding so \nI code everyday!')
+# print is a useful way to figure out whats goining on when debugging, it helps find where you have problems on your code
