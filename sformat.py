fe76eaf8b32bacfcff1950133197f908ba51dffa
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 00:46:43 -0600

    Add more content to file

--- sformat.py
+++ sformat.py
@@ -1,5 +1,6 @@
 # several ways to custom  string formatting
 first_name = input('Please  enter your first name: ')
 last_name = input('Please  enter  your last name: ')
-output =  'Welcome ' + first_name + ' ' + last_name + ' ' + 'to my page!'
+# output =  'Welcome ' + first_name + ' ' + last_name + ' ' + 'to my page!'
+output =  'Welcome  {} {}' .format(first_name, last_name)
 print(output)
