5d123d95b9f63c7069a6b588724fff1391c3d09f
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-20 23:55:41 -0600

    Add more content to file

--- strings.py
+++ strings.py
@@ -2,3 +2,6 @@ first_name = 'John'
 last_name = 'Doe'
 print(first_name + ' ' + last_name)
 print('Hello ' + first_name + ' ' + last_name)
+# Using function to modify strings
+express = 'I feel great today!'
+print(express.upper())
