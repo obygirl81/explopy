c4192068cbe42e3f723f23ff1a40bc5cf2a15d44
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 01:06:19 -0600

    Add a new file

---
+++ num.py
@@ -0,0 +1,8 @@
+first_num  = 5
+second_num =  8
+print(first_num + second_num)
+# all Operation uses  same math symbol except Exponent. Exponent uses **
+print(first_num - second_num)
+print(first_num * second_num)
+print(first_num  /  second_num)
+print(first_num **  second_num)
