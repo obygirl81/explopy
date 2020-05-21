1accec6eaf0de715f756f055d62f59842b331a21
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-20 23:58:47 -0600

    Add more content to file

--- strings.py
+++ strings.py
@@ -3,5 +3,8 @@ last_name = 'Doe'
 print(first_name + ' ' + last_name)
 print('Hello ' + first_name + ' ' + last_name)
 # Using function to modify strings
-express = 'I feel great today!'
+express = 'i feel great today!'
 print(express.upper())
+print(express.lower())
+print(express.capitalize())
+print(express.count('e'))
