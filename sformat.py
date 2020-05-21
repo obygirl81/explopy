fec2359d054ac90d70fff3a2e6c37c689b796761
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 00:51:30 -0600

    Add new content to file

--- sformat.py
+++ sformat.py
@@ -2,5 +2,6 @@
 first_name = input('Please  enter your first name: ')
 last_name = input('Please  enter  your last name: ')
 # output =  'Welcome ' + first_name + ' ' + last_name + ' ' + 'to my page!'
-output =  'Welcome  {} {}' .format(first_name, last_name)
+# output = 'Welcome  {} {}' .format(first_name, last_name)
+output = 'Welcome  {0} {1}'.format(first_name, last_name)
 print(output)
