ebc7fd77c1dcf63036b00f82d24eeb2342dd6e4e
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 01:16:28 -0600

    Add more content to file

--- num.py
+++ num.py
@@ -1,8 +1,15 @@
 first_num  = 5
 second_num =  8
 print(first_num + second_num)
-# all Operation uses  same math symbol except Exponent. Exponent uses **
+# all Operation use the same math symbol known  to all except Exponent. Exponent uses **
 print(first_num - second_num)
 print(first_num * second_num)
 print(first_num  /  second_num)
 print(first_num **  second_num)
+# Python gets confused when strings are combined  with numbers so we have to do type conversion. Example below shows the conversion of a number to a string
+days_in_feb = 28
+print(str(days_in_feb) + ' days in February')
+# Numbers stored as strings are treated as strings and when added together, the first value is concatenated to the second value. See below:
+first_num = '8'
+second_num = '10'
+print(first_num + second_num)
