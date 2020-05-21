f68211d3c512e42ec7810ea8143f857cc9566609
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 13:41:47 -0600

    Add more content to file

--- dates.py
+++ dates.py
@@ -1,6 +1,13 @@
 # We need the current date and time when working with date so to get the current date and time, we use the datetime library
-from  datetime import datetime
+from  datetime import datetime, timedelta
 
 current_date = datetime.now()
 # the now function returns a datetime object see below
 print('The date today is: ' +  str(current_date))
+# Below are functions we can use with datetime objects to manipulate dates 
+# the timedelta is used to define a period of time
+today =  datetime.now()
+print('Today is: ' + str(today))
+one_day = timedelta(days=1)
+yesterday = today - one_day
+print('Yesterday was: ' + str(yesterday))
