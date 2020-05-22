55e14afec66a4f7b19d35a199935f46c54c07e77
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 00:29:16 -0600

    Add multi if statements

--- multicon.py
+++ multicon.py
@@ -1,1 +1,11 @@
 # Checking multiple conditions to determine the correct action
+city = input('What city do you live in? ')
+tax = 0
+
+if city == 'Denver':
+    tax = 0.08
+if city == 'Aurora':
+    tax = 0.07
+if city == 'Englewood':
+    tax = 0.09
+print(tax)
