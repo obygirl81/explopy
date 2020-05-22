3a3eadcdf089b69c206d2ffe61189eac4278261c
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 23:59:07 -0600

    Add string comparison

--- price.py
+++ price.py
@@ -3,7 +3,13 @@ price = input('How much did you pay? ')
 price = float(price)
 if price >= 1.00:
     tax = .07
-    print('Your tax rate is ' + str(tax))
 else:
     tax = 0
-    print('Your tax rate is ' + str(tax))
+print('Your tax rate is ' + str(tax))
+
+# comparing strings
+hobby = input('What is your favorite hobby? ')
+if hobby == 'tennis':
+    print("yea! Me too!")
+else:
+    print("I'd like to try that sometime")
