918a765da2ab06f2eb2d03310908bb8dca5b7778
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 22:22:03 -0600

    Add syntax error

---
+++ error.py
@@ -0,0 +1,5 @@
+# Synatx errors, runtime errors, and logic errors are error types in python
+#Below is an exmaple of a syntax error. Our code wont run at all with syntax error
+x = 20
+y = 102
+if x == print('Success')
