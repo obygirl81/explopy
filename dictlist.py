c1e29bd58365f64043b1bcadb4951ae6c062f6c0
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 14:26:48 -0600

    Update variable name

--- dictlist.py
+++ dictlist.py
@@ -1,14 +1,14 @@
-laura = {} # empty  dictionary or place holder. Can use to create an object
+laura = {} # empty  dictionary or place holder. Can use to create an object on the fly
 laura['first'] = 'Laura'
 laura['last'] = 'Mongo'
 
-Mya = {} 
-Mya['first'] = 'Mya'
-Mya['last'] = 'Node'
+mya = {} 
+mya['first'] = 'Mya'
+mya['last'] = 'Node'
 
-people = []  # empty list or place holder . Can use list to add multiple things
+people = []  # empty list or place holder . Can use list to store multiple things including adding other dictionaries
 people.append(laura)
-people.append(Mya)
+people.append(mya)
 print(people)
 
 people.append({
