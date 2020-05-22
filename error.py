4902120f46386d1ad900b1ba3a64b8358c23910d
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 22:27:56 -0600

    Add runtime error to file

--- error.py
+++ error.py
@@ -1,5 +1,9 @@
 # Synatx errors, runtime errors, and logic errors are error types in python
 #Below is an exmaple of a syntax error. Our code wont run at all with syntax error
-x = 20
-y = 102
-if x == print('Success')
+# x = 20
+# y = 102
+# if x == print('Success')
+# Runtime errors fail when run. Below is an example. It usually will give you alittle info on where to start trying to debug your code
+x = 22
+y = 0
+print(x / y)
