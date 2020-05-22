a8cdc3d05736ca3e9d65d20ccdb7d0b492ffb236
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 01:19:24 -0600

    Add and statement

--- complex.py
+++ complex.py
@@ -6,6 +6,5 @@ gpa = float(input('What was your Grade Point Average? '))
 # lowest_grade = float(lowest_grade)
 lowest_grade = float(input('What was your lowest grade? '))
 
-if gpa >= .85:
-    if lowest_grade >= .70:
+if gpa >= .85 and lowest_grade >= .70: # used the and statement here
         print('You made the honour roll!')
