a23bb0868e3cf11c2a7813b7f152bcb6df4843ea
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 15:04:30 -0600

    Add while loop

--- fornwhile.py
+++ fornwhile.py
@@ -1,5 +1,10 @@
 # using for loop and while loop
 people = ['Mike', 'Susan', 'Logan']
 
-for name in people:
-    print(name)
+print() # just putting an empty line
+# for name in people:
+#     print(name)
+index = 0
+while index < len(people):
+    print(people[index])
+    index = index + 1
