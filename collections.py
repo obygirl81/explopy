93cbc3c56660dd07f8e86ada9c2a179e884c509a
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 01:58:03 -0600

    Add lists to file

---
+++ collections.py
@@ -0,0 +1,11 @@
+# Reviewing Lists and Arrays
+# List are collection of items and ser set up using square brackets []. these items can be objects, strings, numbers etc
+names = ['sara', 'John']
+scores = []
+scores.append(99) # Adds new item to the end
+scores.append(100)
+print(names)
+print(scores)
+print(scores[0])
+print(scores[1])
+# Arrays are also collection of items. To use an array, you have to create an array object and you get them by bringing them in from array library
