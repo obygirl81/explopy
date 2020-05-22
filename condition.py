997b4cb03db096dec8429913a16d11283365487a
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 23:23:10 -0600

    Add another  way to write if else statement

--- condition.py
+++ condition.py
@@ -1,9 +1,15 @@
 # Taking different actions based on different conditions. See example below:
-price = .05
+price = 1.05
+# if price >= 1.00:
+#     tax = .07
+#     print(tax)
+# # adding a default action using else
+# else:
+#     tax = 0
+#     print(tax)    
+# Another way of writing if else statement is shown below:
 if price >= 1.00:
-    tax = .07
-    print(tax)
-# adding a default action using else
+    tax = 0.07
 else:
     tax = 0
-    print(tax)    
+print(tax)
