c374b136eee610f615758127388804750b58d715
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 14:42:32 -0600

    Add range function

--- loops.py
+++ loops.py
@@ -5,3 +5,7 @@ for name in ['Laura', 'Carey']:
 print()    
 for name in ['Orange', 'Apple', 'Grape']:
     print(name)
+    
+# When looping a number of times,use the built in range function.  Range will automatically create a list of numbers for you.
+for index in range(0,2): # here 0 is the strting number while 2 is the number of items that we'll  get
+    print(index)
