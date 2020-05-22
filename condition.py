5f23c28bcbcde175e91cfd17ac33e62a4d944a35
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-21 23:17:03 -0600

    Add a default action using else

--- condition.py
+++ condition.py
@@ -1,5 +1,9 @@
 # Taking different actions based on different conditions. See example below:
-price = 1.00
+price = .05
 if price >= 1.00:
     tax = .07
     print(tax)
+# adding a default action using else
+else:
+    tax = 0
+    print(tax)    
