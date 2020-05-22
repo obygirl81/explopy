536ecd53615bb4ef4657133689e4270af7206a35
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 00:32:13 -0600

    Add elif statement

--- multicon.py
+++ multicon.py
@@ -4,8 +4,8 @@ tax = 0
 
 if city == 'Denver':
     tax = 0.08
-if city == 'Aurora':
+elif city == 'Aurora':
     tax = 0.07
-if city == 'Englewood':
+elif city == 'Englewood':
     tax = 0.09
 print(tax)
