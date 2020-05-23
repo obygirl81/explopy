53d16b18bc6a93f4b13217ca1d67b8168c86153d
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 23:09:40 -0600

    Add named notation

--- params.py
+++ params.py
@@ -7,6 +7,8 @@ def get_initial(name, force_uppercase=True): # added a boolean value so I dont h
     return initial
 
 first_name = input('Enter your first name: ')
-first_name_initial = get_initial(first_name) # removed the previous False value
+# first_name_initial = get_initial(first_name) # removed the previous False value
+# using a named Notation
+first_name_initial = get_initial(force_uppercase=False, name=first_name)
 
 print('Your initial is: ' + first_name_initial)
