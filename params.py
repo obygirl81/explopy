a37cedd72250d90dfcbe0e03cf376d7b0ace302e
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 23:06:53 -0600

    Add boolean value as a parameter

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 # Using multiple parameters in functions
-def get_initial(name, force_uppercase):
+def get_initial(name, force_uppercase=True): # added a boolean value so I dont have to pass in another in line 10. Although I can still leave value and code will stil run
     if force_uppercase:
         initial = name[0:1].upper()
     else:
@@ -7,6 +7,6 @@ def get_initial(name, force_uppercase):
     return initial
 
 first_name = input('Enter your first name: ')
-first_name_initial = get_initial(first_name, False)
+first_name_initial = get_initial(first_name) # removed the previous False value
 
 print('Your initial is: ' + first_name_initial)
