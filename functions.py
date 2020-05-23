d099a61be0b66390aa3df1e8c1b58acf5f800e00
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 18:17:06 -0600

    Add functions file

---
+++ functions.py
@@ -0,0 +1,14 @@
+# Using functions is a way to write code efiiciently especially since we sometimes copy and paste our code
+# printing timestamps to see how long sections take to run
+import datetime
+
+first_name = 'Gloria'
+print('task completed')
+print(datetime.datetime.now())
+print()
+
+for x in range(0,10):
+    print(x)
+print('task completed')
+print(datetime.datetime.now())
+print()
