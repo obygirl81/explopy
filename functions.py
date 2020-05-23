4653f82b168d513e8a118a50e570866731538df1
Obiageli Ogbutor <obygirl81@gmail.com>
2020-05-22 22:01:19 -0600

    Add parameter in function  file

--- functions.py
+++ functions.py
@@ -15,16 +15,29 @@ from datetime import datetime
 
 # Using functions - its better to write a function and call it when you need it rather than continous copying and pasting code. See example below:
 # Print the current time
-def print_time():
-    print('task completed')
-    # print(datetime.datetime.now())
-    print(datetime.now()) # a better way of rewriting the code i have on line 20 since I adjusted teh statement on line 3
+# def print_time():
+#     print('task completed')
+#     # print(datetime.datetime.now())
+#     print(datetime.now()) # a better way of rewriting the code i have on line 20 since I adjusted teh statement on line 3
+#     print()
+    
+# first_name = 'Ragnar'
+# print_time()
+
+# for x in range(0,10):
+#     print(x)
+# print_time()
+    
+# Passing parameter in functions
+# Printing current time and task name
+def print_time(task_name):
+    print(task_name)
+    print(datetime.now())
     print()
     
-first_name = 'Ragnar'
-print_time()
+first_name = 'Yolanda'
+print_time('first name as assigned')
 
 for x in range(0,10):
     print(x)
-print_time()
-    
+print_time('loop completed')
